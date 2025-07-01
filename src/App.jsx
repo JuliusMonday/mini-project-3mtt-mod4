08dc8bf8d55d3ed7ae823d937ccfdbab6c90bc42
JuliusMonday <julietech3@gmail.com>
2025-07-01 06:39:48 +0100

    Working on my app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import ListComponent from './components/ListComponent';
+    const App = () => {
+      const [characters, setCharacters] = useState([]);
+      const [loading, setLoading] = useState(true);
+      const [error, setError] = useState(null);
+
+      React.useEffect(() => {
+        setLoading(true);
+        fetch('https://rickandmortyapi.com/api/character')
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+            return response.json();
+          })
+          .then(data => {
+            setCharacters(data.results);
+            setLoading(false);
+          })
+          .catch(error => {
+            setError(error.message);
+            setLoading(false);
+          });
+      }, []);
+
+      if (loading) {
+        return <p>Loading...</p>;
+      }
+
+      if (error) {
+        return <p>Error: {error}</p>;
+      }
+
+      return (
+        <div>
+          <h1>Rick and Morty Characters</h1>
+          <ListComponent
+            items={characters}
+            renderItem={character => (
+              <div className='character'>
+                <img className='character-image'
+                  src={character.image} 
+                  alt={character.name} 
+                />
+                <p className='character-name'>{character.name}</p>
+              </div>
+            )}
+          />
+        </div>
+      );
+    };
+
+ export default App;
